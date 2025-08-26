3f683c9b09f4e39cad4d5c4968e7b25af9c66fcc
iamadarshkohale <adarshkohale23@gmail.com>
2025-08-26 08:12:40 +0530

    added files

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my-first_instance" {
+  ami = "ami-0861f4e788f5069dd"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "web-server"
+  }
+}
